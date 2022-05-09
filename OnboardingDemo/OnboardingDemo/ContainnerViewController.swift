b799f0d1e9b2c110d70e584e2f9cd1db3eacc0b5
joelzarco <joelzarco@me.com>
2022-05-09 17:14:59 -0500

    Template page for onboarding added

---
+++ OnboardingDemo/OnboardingDemo/ContainnerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnboardingDemo
+//
+//  Created by Johel Zarco on 09/05/22.
+//
+
+import UIKit
+
+class ContainnerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OnboardingDemo/OnboardingDemo/OnboardingViewController.swift
@@ -0,0 +1,72 @@
+//
+//  OnboardingViewController.swift
+//  OnboardingDemo
+//
+//  Created by Johel Zarco on 09/05/22.
+// Template page for Onboarding
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    
+    let stackView = UIStackView()
+    let emojiView = UILabel()
+    let titleLabel = UILabel()
+    
+    
+    let emoji: String
+    let titleText: String
+    let emojiHeigt : CGFloat = 250
+    let spacing : CGFloat = 20
+    
+    init(emoji: String, titleText: String) {
+        self.emoji = emoji
+        self.titleText = titleText
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented - not using storyboards")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension OnboardingViewController {
+    func style() {
+        
+        view.backgroundColor = .systemBackground
+        // bigEmoji
+        emojiView.translatesAutoresizingMaskIntoConstraints = false
+        emojiView.contentMode = .scaleAspectFit
+        emojiView.font = UIFont.systemFont(ofSize: emojiHeigt)
+        emojiView.text = emoji
+        
+        // Label
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont.preferredFont(forTextStyle: .title3)
+        titleLabel.adjustsFontForContentSizeCategory = true
+        titleLabel.numberOfLines = 0
+        titleLabel.text = titleText
+    }
+    
+    func layout() {
+
+        view.addSubview(emojiView)
+        view.addSubview(titleLabel)
+
+        emojiView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        emojiView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: emojiView.bottomAnchor, constant: spacing).isActive = true
+        
+    }
+}
+
